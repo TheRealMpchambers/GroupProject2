d80ee862d22c0207ad3b100fd3724ea89f97dc05
unknown <jodimcarlson4@gmail.com>
2019-01-12 12:38:58 -0600

    updated routes folder files

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,19 +3,19 @@ var db = require("../models");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
+    db.Schedule.findAll({}).then(function(dbSchedule) {
       res.render("index", {
         msg: "Welcome!",
-        examples: dbExamples
+        examples: dbSchedule
       });
     });
   });
 
   // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+  app.get("/schedule/:id", function(req, res) {
+    db.Schedule.findOne({ where: { id: req.params.id } }).then(function(dbSchedule) {
       res.render("example", {
-        example: dbExample
+        example: dbSchedule
       });
     });
   });
