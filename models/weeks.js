70bc2209c490f9b4dd269db865bf9b7cfff02c95
Kevin <kobbootcamp@gmail.com>
2019-01-16 17:41:50 -0600

    Getting right

--- models/weeks.js
+++ models/weeks.js
@@ -1,7 +1,7 @@
-module.exports = function(sequelize, DataTypes) {
-    var Weeks = sequelize.define("weeks", {
-      StartDate: DataTypes.DATE,
-      EndDate: DataTypes.DATE
-    });
-    return Weeks;
-  };
+module.exports = function (sequelize, DataTypes) {
+  var Weeks = sequelize.define("weeks", {
+    text: DataTypes.STRING,
+    description: DataTypes.TEXT
+  });
+  return Weeks;
+};
