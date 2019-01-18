7094238c292b39aa458bbc6c82c53b395cdc8b2b
That_Guy <eghove@gmail.com>
2019-01-18 10:41:19 -0600

    seed file for weeks

---
+++ models/sql/weeks_table_seeds.sql
@@ -0,0 +1,40 @@
+USE cabin_db;
+
+INSERT INTO weeks (StartDate, EndDate, Available, createdAt, updatedAt)
+VALUES
+(
+"2019-01-01",
+"2019-01-08",
+true,
+now(),
+now()
+);
+
+USE cabin_db;
+
+INSERT INTO weeks (StartDate, EndDate, Available, createdAt, updatedAt)
+VALUES
+(
+"2019-01-09",
+"2019-01-16",
+true,
+now(),
+now()
+),
+(
+"2019-01-17",
+"2019-01-24",
+true,
+now(),
+now()
+),
+(
+"2019-01-25",
+"2019-02-01",
+true,
+now(),
+now()
+);
+
+
+SELECT * FROM weeks;
